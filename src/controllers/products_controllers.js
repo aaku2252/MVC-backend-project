9975a0471a32a12d6fd3337264e61700d7c89ab1
Akash <akkyjat601@gmail.com>
2023-11-22 18:53:28 +0530

    ejs module

--- src/controllers/products_controllers.js
+++ src/controllers/products_controllers.js
@@ -5,7 +5,7 @@ export default class ProductController {
     getProducts(req, res) {
         let products = ProductModel.get();
         // return res.sendFile(
-        //     path.join(path.resolve(), "src", "views", "products.ejs")
+        //     path.join(path.resolve(), "src", "views", "products.html")
         // );
         res.render("products.ejs", { products: products });
     }
